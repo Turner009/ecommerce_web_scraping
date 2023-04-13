d2c2638ae234f1836e7e7933c83a66ca811f14be
Turner009 <009turner@gmail.com>
2023-04-12 23:01:15 -0500

    complete Project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ecommerce_web_scraping
+
+What was the biggest challenge of scraping the web site?
+    
+    The biggest challenge in scraping the website was dealing with the popup that asked to accept cookies.    
+
+What insight, if any, did you gain from the analysis and visualization of the laptop data?
+
+    The insight I gained from the visualization is that most laptop prices fall between the  $200 - $600 or $1000 - $1400 range with few  options in between or above those ranges. There does not seem to be any correlation between price and number of reviews. I would have thought the more affordable items would have had more reviews. Due to having more sales.
--- ecommerce _scraping_selenium.py
+++ ecommerce _scraping_selenium.py
@@ -1,5 +1,43 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-import csv
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+import csv, operator
 
+file = open('computers_and_laptops.csv', 'w', newline='')
+writer = csv.writer(file)
+writer.writerow(["id", "name", "price", "specifications", "number of reviews"])
+
+browser_driver = Service('/Users/mptur/Downloads/chromedriver.exe')
+scraper = webdriver.Chrome(service=browser_driver)
+
+scraper.get('https://webscraper.io/test-sites/e-commerce/static/computers/laptops')
+
+element = scraper.find_element(By.CLASS_NAME, 'acceptCookies').click()
+
+id = 1
+while True:
+    devices = scraper.find_elements(By.CLASS_NAME, 'thumbnail')
+
+    for items in devices:
+        name = items.find_element(By.CLASS_NAME, 'title').text
+        price = items.find_element(By.CLASS_NAME, 'pull-right.price').text
+        price = float(price[1:])
+        specs = items.find_element(By.CLASS_NAME, 'description').text
+        reviews = items.find_element(By.CLASS_NAME, 'ratings').text
+        reviews = int(reviews[:-7])
+        writer.writerow([id, name, price, specs, reviews])
+        id +=1
+        
+        
+
+    try:
+        element = scraper.find_element(By.XPATH, '/html/body/div[1]/div[3]/div/div[2]/nav/ul/li[15]/a')
+        element.click()
+    except NoSuchElementException:
+        break
+
+file = sorted(file, key=lambda row:row[2])
+file.close()
+scraper.quit
